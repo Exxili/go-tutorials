e432e02333f0046536bb15ea29e3652ae00dd211
Exxili <sirvaux@live.com>
2022-01-24 09:06:32 +0000

    implemented error checking in greetings module and tested within helloModule

--- helloModule/helloModule.go
+++ helloModule/helloModule.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example/greetings"
 )
 
 func main() {
+	// Set a prefix for the logger
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// Get a greeting message
-	message := greetings.Hello("Andy")
+	message, err := greetings.Hello("")
+
+	// If there is an error, print to console and exit
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// if no Err, print returned message
 	fmt.Println(message)
 }
